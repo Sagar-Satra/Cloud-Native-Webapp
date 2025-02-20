daa1dae4784db11770eca995d04343c4033def0b
Sagar-Satra <satra.sa@northeastern.edu>
2025-02-19 19:31:05 -0500

    updated the port

--- app/database/db.js
+++ app/database/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME , process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
   define : {
     timestamps : false
