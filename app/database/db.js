cce3b765145ce7df088ec0e34361a988c7b3d51d
Sagar-Satra <satra.sa@northeastern.edu>
2025-02-19 19:46:43 -0500

    updated the mysql port

--- app/database/db.js
+++ app/database/db.js
@@ -11,4 +11,14 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME , p
   }
 });
 
+// Method to verify the connection
+export const authenticate = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
 export default sequelize;
