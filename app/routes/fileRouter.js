ea3475ff8dd58bcf3a9c6d2250c3ca9fa43eba0a
Sagar-Satra <satra.sa@northeastern.edu>
2025-03-20 09:37:00 -0400

    Bug fixes in routers and APIs

--- app/routes/fileRouter.js
+++ app/routes/fileRouter.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import { uploadFile, getFileById, deleteFile, methodsNotAllowed } from '../controllers/fileUploadController.js';
+import sequelize from '../database/db.js';
 
 export const fileRouter = express.Router();
 
@@ -11,14 +12,26 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 } // Limit file size to 5MB
 });
 
+// Middleware to check database connection
+const checkDatabaseConnection = async (req, res, next) => {
+    try {
+      await sequelize.authenticate();
+      next();
+    } catch (error) {
+      console.error('Database connection error:', error);
+      return res.status(503).json({ error: 'Service Unavailable', message: 'Database connection error' });
+    }
+  };
+
 // Handle file uploads - POST /v1/file
 fileRouter.route('/file')
-    .post(upload.single('file'), uploadFile)
+    .post(checkDatabaseConnection, upload.single('file'), uploadFile)
+    .head(methodsNotAllowed)
     .all(methodsNotAllowed);
 
 // Handle file retrieval and deletion - GET & DELETE /v1/file/:id
 fileRouter.route('/file/:id')
-    .get(getFileById)
-    .delete(deleteFile)
+    .get(checkDatabaseConnection, getFileById)
+    .delete(checkDatabaseConnection, deleteFile)
     .head(methodsNotAllowed)
     .all(methodsNotAllowed);
